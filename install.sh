8cd10fded8be7a8dd9406a326f268820ea8266be
Ty Littlefield <tyler@tysdomain.com>
2025-02-28 19:51:38 -0700

    Remove weird sources.d formatting.

--- install.sh
+++ install.sh
@@ -53,9 +53,10 @@ chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker repository
 echo "Adding Docker repository..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \n  https://download.docker.com/linux/debian \n  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # Update package list to pull new docker packages
 echo "Updating package list after adding Docker repo..."
 apt-get update
